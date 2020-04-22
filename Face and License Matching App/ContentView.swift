13fafe3adf9a3deaa3d2566b4440e99993b5358d
Akira Kaawi <kawaiakira@hotmail.com>
2020-04-23 04:09:26 +0900

    Initial Commit

---
+++ Face and License Matching App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Face and License Matching App
+//
+//  Created by 川井 輝 on 2020/04/23.
+//  Copyright © 2020 Akira Kawai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Face and License Matching AppTests/Face_and_License_Matching_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Face_and_License_Matching_AppTests.swift
+//  Face and License Matching AppTests
+//
+//  Created by 川井 輝 on 2020/04/23.
+//  Copyright © 2020 Akira Kawai. All rights reserved.
+//
+
+import XCTest
+@testable import Face_and_License_Matching_App
+
+class Face_and_License_Matching_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
